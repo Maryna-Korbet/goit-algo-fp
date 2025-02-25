ecc474e2d9b72d59289de393d3a8c8bc0cdb695c
Maryna-Korbet <m22v86k@gmail.com>
2025-02-25 00:18:28 -0500

    finished task_1

--- task_1.py
+++ task_1.py
@@ -1,64 +1,115 @@
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, data: int = None):
         self.data = data
-        self.next = None
-
+        self.next: 'Node' | None = None
 
 class LinkedList:
     def __init__(self):
-        self.head = None
+        self.head: Node | None = None
 
-    def insert_at_beginning(self, data):
+    # Insert nodes at the beginning
+    def insert_at_beginning(self, data: int) -> None:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
 
-    def insert_at_end(self, data):
+    # Insert nodes at the end
+    def insert_at_end(self, data: int) -> None:
         new_node = Node(data)
-        if self.head is None:
+        if not self.head:
             self.head = new_node
-        else:
-            cur = self.head
-            while cur.next:
-                cur = cur.next
-            cur.next = new_node
-
-    def insert_after(self, prev_node: Node, data):
-        if prev_node is None:
-            print("The previous node does not exist.")
             return
-        new_node = Node(data)
-        new_node.next = prev_node.next
-        prev_node.next = new_node
-
-    def delete_node(self, key: int):
+        
         cur = self.head
-        if cur and cur.data == key:
-            self.head = cur.next
-            cur = None
-            return
-        prev = None
-        while cur and cur.data != key:
-            prev = cur
+        while cur.next:
             cur = cur.next
-        if cur is None:
-            return
-        prev.next = cur.next
-        cur = None
+        cur.next = new_node
 
+    # Delete a node
+    def delete_node(self, key: int) -> None:
+        cur, prev = self.head, None
+        while cur:
+            if cur.data == key:
+                if prev:
+                    prev.next = cur.next
+                else:
+                    self.head = cur.next
+                return
+            prev, cur = cur, cur.next
+
+    # Search for an element
     def search_element(self, data: int) -> Node | None:
         cur = self.head
         while cur:
             if cur.data == data:
                 return cur
             cur = cur.next
-            return None
+        return None
 
-    def print_list(self):
-        current = self.head
-        while current:
-            print(current.data)
-            current = current.next
+    # Reversing a linked list
+    def reverse(self) -> None:
+        prev, cur = None, self.head
+        while cur:
+            next_node = cur.next
+            cur.next = prev
+            prev, cur = cur, next_node
+        self.head = prev
+
+    # Insert sorting
+    def insertion_sort(self) -> None:
+        if not self.head or not self.head.next:
+            return
+
+        sorted_list = None
+        cur = self.head
+        while cur:
+            next_node = cur.next
+            sorted_list = self._sorted_insert(sorted_list, cur)
+            cur = next_node
+
+        self.head = sorted_list
+
+    #  Insert into a sorted list
+    def _sorted_insert(self, sorted_head: Node | None, new_node: Node) -> Node:
+        if not sorted_head or new_node.data < sorted_head.data:
+            new_node.next = sorted_head
+            return new_node
+
+        cur = sorted_head
+        while cur.next and cur.next.data < new_node.data:
+            cur = cur.next
+
+        new_node.next = cur.next
+        cur.next = new_node
+        return sorted_head
+
+    # Merging two sorted lists
+    def merge_sorted_lists(self, llist2: 'LinkedList') -> None:
+        dummy = Node()
+        tail = dummy
+        l1, l2 = self.head, llist2.head
+
+        while l1 and l2:
+            if l1.data < l2.data:
+                tail.next, l1 = l1, l1.next
+            else:
+                tail.next, l2 = l2, l2.next
+            tail = tail.next
+
+        tail.next = l1 if l1 else l2
+        self.head = dummy.next
+
+    # Print a linked list
+    def print_list(self) -> None:
+        print(self)
+
+    def __str__(self) -> str:
+        result, cur = [], self.head
+        while cur:
+            result.append(str(cur.data))
+            cur = cur.next
+        return " -> ".join(result) + " -> None"
+
 
 llist = LinkedList()
 
@@ -75,7 +126,7 @@ llist.insert_at_end(25)
 print("Linked list:")
 llist.print_list()
 
-# We delete the node
+# Delete the node
 llist.delete_node(10)
 
 print("\nLinked list after removing data node 10:")
@@ -87,3 +138,25 @@ element = llist.search_element(15)
 if element:
     print(element.data)
 
+#  Reversing a linked list
+print("\nReversing linked list:")
+llist.reverse()
+llist.print_list()
+
+# Insert sorting
+print("\nSorting linked list (Linked list 1):")
+llist.insertion_sort()
+llist.print_list()
+
+# Create a second list for merging
+llist2 = LinkedList()
+llist2.insert_at_end(3)
+llist2.insert_at_end(8)
+llist2.insert_at_end(12)
+print("\nLinked list 2:")
+llist2.print_list()
+
+# Merging two sorted lists
+print("\nMerged sorted list:")
+llist.merge_sorted_lists(llist2)
+llist.print_list()
